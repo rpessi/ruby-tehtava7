2d8357d0501736005170151448ebdab3413a0fee
Riitta Pessi <riitta@pessi.fi>
2025-03-14 16:04:56 +0200

    Ruby harjoittelutehtävä 7

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
-# kirjoita koodi tänne
+# Tee metodi keskiarvo, joka saa parametriksi kokonaislukuja sisältävän taulukon ja
+# palauttaa taulukon lukujen keskiarvon. Käytä metodia inject
+
+def keskiarvo(taulukko)
+  taulukko.inject(0) {|summa, x| summa + x}.to_f / taulukko.inject(0) {|kpl, x| kpl + 1}
+end
+
+puts keskiarvo([1, 2, 4, 9])
+puts keskiarvo([1, 2])
